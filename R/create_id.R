984c82351551904d6ddc4b3fcb9617eb026f3ba6
Mike Guron <mike_guron@alumni.ubc.ca>
2023-01-20 21:46:25 -0800

    Add roxygen comments for create_id fucntion

--- R/create_id.R
+++ R/create_id.R
@@ -1,3 +1,16 @@
+#' Create ID
+#'
+#' Convert a vector of provided urls into a vector of unique identifiers for use in downstream functions.
+#'
+#' @param urls A vector of urls as strings.
+#'
+#' @return A vector of unique identifiers as strings.
+#' @export
+#'
+#' @examples
+#' urls <- c('https://www.reddit.com/r/nba/', 'https://www.reddit.com/r/nfl/', 'https://vancouver.craigslist.org/search/apa', 'https://www.kijiji.ca/b-real-estate/richmond-bc/c34l1700288')
+#' create_id(urls)
+#' [1] 'reddit1'  'reddit2'  'craigslist1'  'kijiji1'
 create_id <- function(urls){
   #code to be written
 }
