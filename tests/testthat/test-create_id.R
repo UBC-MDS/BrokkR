7690b2e2c473fe9fa58f95b77f719607a4d6c7d7
Mike Guron <mike_guron@alumni.ubc.ca>
2023-01-27 19:07:32 -0800

    Add file containing unit tests for create_id function

---
+++ tests/testthat/test-create_id.R
@@ -0,0 +1,26 @@
+#' Unit tests for create_id() function of BrokkR
+
+#' Confirms the input is a character vector
+test_that("input is a character vector", {
+  expect_type(urls, "character")
+})
+
+#' Confirms the output is a character vector
+test_that("output is a character vector", {
+  expect_type(actual, "character")
+})
+
+#' Confirms the output vector of identifiers is the same length as the input vector of urls
+test_that("input and output vectors are of same length", {
+  expect_equal(length(urls), length(actual))
+})
+
+#' Confirms all identifers are unique and no duplicates were generated
+test_that("output identifiers are unique", {
+  expect_equal(length(unique(actual)), length(actual))
+})
+
+#' Confirms the actual output matches the expected output of the function
+test_that("input and output vectors match", {
+  expect_identical(expected, actual)
+})  
