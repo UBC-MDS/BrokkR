c6ca8567479041c6912b38c68754209233891ae2
Mike Guron <mike_guron@alumni.ubc.ca>
2023-01-27 19:03:53 -0800

    Update code documentation to match inter code function

--- R/create_id.R
+++ R/create_id.R
@@ -1,16 +1,16 @@
 #' Create ID
 #'
-#' Convert a vector of provided urls into a vector of unique identifiers for use in downstream functions.
+#' Convert a character vector of provided urls into a character vector of unique identifiers for use in downstream functions.
 #'
-#' @param urls A vector of urls as strings.
+#' @param urls A character vector of urls.
 #'
-#' @return A vector of unique identifiers as strings.
+#' @return A character vector of unique identifiers.
 #' @export
 #'
 #' @examples
-#' urls <- c('https://www.reddit.com/r/nba/', 'https://www.reddit.com/r/nfl/', 'https://vancouver.craigslist.org/search/apa', 'https://www.kijiji.ca/b-real-estate/richmond-bc/c34l1700288')
+#' urls <- c('https://www.reddit.com/r/nba/', 'https://www.reddit.com/r/nfl/', 'https://vancouver.craigslist.org/search/apa', 'https://www.kaggle.com/search?q=nba')
 #' create_id(urls)
-#' [1] 'reddit1'  'reddit2'  'craigslist1'  'kijiji1'
+#' [1] 'reddit'  'reddit.1'  'craigslist'  'kaggle'
 create_id <- function(urls){
   ids <- c()
   for (url in urls) {
