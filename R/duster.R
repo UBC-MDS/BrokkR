60d3583e94a70d92c6035396d55572780832c353
Elena Ganacheva <elena.ganacheva@gmail.com>
2023-01-28 19:23:44 -0800

    Added tests for duster

--- R/duster.R
+++ R/duster.R
@@ -18,8 +18,13 @@
 library(dplyr)
 library(stringr)
 
-duster <- function(urls){
-  text <- brok_scrape(urls) |> unlist() |> str_replace_all("\n", " ")
+duster <- function(urls, test=FALSE){
+  if(test==TRUE){
+    text <- list("Fake Python\n\nFake Jobs for Your Web Scraping Journey\n\nEnergy engineer\nVasquez-Davidson\n\nParty prevent live. Quickly candidate change although. Together type music hospital. Every speech support time operation wear often.\n\nLocation: Christopherville, AA\n\nPosted: 2021-04-08",
+                 "Fake Python\n\nFake Jobs for Your Web Scraping Journey\n\nMaterials engineer\nDavis, Serrano and Cook\n\nApproach great top series public none. Include air sort couple hold group but. Again identify real to follow so. Live teach movie I situation understand agree.\n\nLocation: South Tammyberg, AP\n\nPosted: 2021-04-08")
+  }else{
+    text <- brok_scrape(urls) |> unlist() |> str_replace_all("\n", " ")
+  }
   ids <- create_id(urls)
   df <- tibble(id = ids,
                raw_url = urls,
